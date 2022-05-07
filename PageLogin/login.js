8f92c67248b4db68cbeb59d1a1a4c586b19519e3
FlaviaMedina <flomedina07@gmail.com>
2022-05-06 22:15:38 -0300

    agregado cambio login y recuperacion password

--- PageLogin/login.js
+++ PageLogin/login.js
@@ -1,90 +1,63 @@
 // loguin con validacion
-const botonIngresar = document.getElementById("ingrersar");
-const botonResetContraseña = document.getElementById("cambiarContraseña");
+const botonIngresar = document.getElementById("ingresar");
 const resultado = document.querySelector(".resultado");
-let email = document.getElementById("loguinEmail");
-let contraseña = document.getElementById("loguinContraseña");
-let emailR= document.getElementById("loguinEmailR");
-let contrseñaR = document.getElementById("loguinContraseñaR");
-//ingresar
+const email = document.getElementById("loguinEmail");
+const contraseña = document.getElementById("loguinContraseña");
+
+
+const obtenerUsuario = ()=>{
+  fetch('http://localhost:3000/users')
+  .then(response=>response.json())
+  .then(response => {
+     for (let i = 0; i < response.length; i++) {
+       const arrayRecorrido= response[i];
+       console.log(arrayRecorrido);
+      if(arrayRecorrido.email == email.value && arrayRecorrido.contraseña == contraseña.value
+        && arrayRecorrido.profile == "user"){
+        window.location = "home.html";
+        return true;
+      }else if(arrayRecorrido.email == email.value && arrayRecorrido.contraseña == contraseña.value
+        && arrayRecorrido.profile == "admin" ){
+          window.location = "administracion.html";
+          return true;
+        }
+      else{ 
+        return false;
+      }  
+     } 
+  })
+}
+//iniciar secion
 botonIngresar.addEventListener("click",(e)=>{
   e.preventDefault();
-  let error = validarCampos();
-  if(error[0]){
-    resultado.innerHTML = error[1];
+  let errors = validarCampos();
+  if(errors.length > 0){
+    resultado.innerHTML = errors;
     resultado.classList.add("red");
   }else{
     resultado.innerHTML = "Solicitud correcta";
     resultado.classList.add("green");
+    obtenerUsuario();
   }
  });
  
 const validarCampos = ()=>{
 const regExpEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
 let error =[];
-  if (email.value.lenght < 5 || email.value.lenght>50 
-    ||regExpEmail.test(email.value)){
-     error[0]= true;
-     error[1] = "El email es invalido";
-     return error;
-   } else if(contraseña.value == " " || contraseña.value.lenght < 4 || contraseña.value.lenght >20) {
-    error[0]= true;
-    error[1] = "La contraseña debe ser mayor a cuatro caracteres";
-    return error; }
-    else if(email.value == recorrerRegistro.email.value && contraseña.value == recorrerRegistro.email.value
-       && recorrerRegistro.profile.value == "user" ){
-    window.location = "home.html";
-   }else if(email.value == recorrerRegistro.email.value && contraseña.value == recorrerRegistro.email.value
-    && recorrerRegistro.profile.value == "admin" ){
-      window.location = "administracion.html";
-   }
-   else{
-     error[0]= true;
-     error[1]="Ingreso no valido";
-     return error;
+if (regExpEmail.test(email.value)){
+    error.push({error:true,msg:"El email es invalido"}) ; 
    }  
-   error[0]= false;
-   return error; 
+   if(contraseña.value == " " || contraseña.value.lenght < 4 || contraseña.value.lenght >20) {
+    error.push({error:true,msg:"La contraseña debe ser mayor a cuatro caracteres"}) ;
+  } 
+  return error;
  }
  function mayus(e) {
   e.value = e.value.toUpperCase();
 }
 
-//recuperacion contraseña
-botonResetContraseña.addEventListener("click",(e)=>{
-  e.preventDefault();
-  Comparar();
-  if (recorrerRegistro.email.value == emailR.value) {
-    modificarContraseña();
-  }
-});
 
-function Comparar(){
-  for (let i = 0; i < obtenerUsuario().lenght; i++) {
-  let recorrerRegistro = obtenerUsuario()[i]; 
-  console.log(recorrerRegistro); 
-}
-}
 
-//fech
-function obtenerUsuario(){
-fech('http://localhost:3000/users')
-.then(response=>response.jason())
-.then(response=>console.log(response))
-}
 
-function modificarContraseña(){
-  fech('http://localhost:3000/users',{
-    method:'PATH',
-    body: JSON.stringify({
-      "password":contraseñaR.value,
-      "profile":"user"}),
-      Headers:{
-        'content-type': 'aplication/jason; charset=UTF8'
-      }
-    })
-    .then(response=>response.jason())
-    .then(response=>console.log(response))
-    }
   
     
