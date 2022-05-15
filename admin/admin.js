a11034beb98a3be6fc5744345af7f2a7feb42296
Gaston Avila <gastonavila14@gmail.com>
2022-05-15 20:42:07 -0300

    Admin: Ventana modal, optimizacion de codigo

--- admin/admin.js
+++ admin/admin.js
@@ -6,48 +6,64 @@ let initialStatusChanged = false;
 const getIdNumber = (idString) => {
   return idString.split("-")[1];
 };
+
+const eventChangeCheck = (e, previousFavId) => {
+  if (e.target.id != previousFavId) {
+    if (dbInfoToChange.find((item) => item.newFavId)) {
+      dbInfoToChange[dbInfoToChange.length - 1].newFavId = e.target.id;
+    } else {
+      dbInfoToChange.push({ newFavId: e.target.id });
+    }
+    btnSave.classList.remove("btn-save-hide");
+    btnSave.classList.add("btn-save");
+    initialStatusChanged = true;
+  } else {
+    btnSave.classList.add("btn-save-hide");
+    btnSave.classList.remove("btn-save");
+  }
+};
+
 const getAllData = async (endpoint) => {
-  let response = await fetch(`http://localhost:3000/${endpoint}`);
-  let items = await response.json();
-  items.forEach((item) => {
-    dbInitialStatus.push({ id: `radio-${item.id}`, fav: item.Principal });
-    initialStatusChanged = false;
-    tableDataRows.innerHTML += `<tr ${
-      !item.Available ? 'class="table-danger"' : ""
-    }>
-    <th scope="row">${item.id}</th>
-    <td>${item.Title}</td>
-    <td>${item.Year}</td>
-    <td>${item.Type === "movie" ? "Pelicula" : "Serie"}</td>
-    <td>${item.Available ? "Si" : "No"}</td>
-    <td><input type="radio" ${item.Principal ? `checked` : " "} 
-        ${!item.Available ? "disabled" : " "}
-        class="fav-movie-radio" name="fav-movie-group" id="radio-${
-          item.id
-        }"></td>
-        </tr>`;
-  });
-  let { id: previousFavId } = dbInitialStatus.find((item) => item.fav);
-  dbInfoToChange.push({ previousFavId });
-  checksMovieFav = document.querySelectorAll(".fav-movie-radio");
-  checksMovieFav.forEach((check) => {
-    check.addEventListener("change", (e) => {
-      if (e.target.id != previousFavId) {
-        if (dbInfoToChange.find((item) => item.newFavId)) {
-          dbInfoToChange[dbInfoToChange.length - 1].newFavId = e.target.id;
-        } else {
-          dbInfoToChange.push({ newFavId: e.target.id });
-        }
-        btnSave.classList.remove("btn-save-hide");
-        btnSave.classList.add("btn-save");
-      } else {
-        btnSave.classList.add("btn-save-hide");
-        btnSave.classList.remove("btn-save");
-      }
-      // console.log(btnSave)
-      console.log(e.target.id);
+  try {
+    let response = await fetch(`http://localhost:3000/${endpoint}`);
+    let items = await response.json();
+    items.forEach((item) => {
+      dbInitialStatus.push({
+        id: `radio-${item.id}`,
+        fav: item.Principal,
+        Title: item.Title,
+        Year: item.Year,
+        Genre: item.Genre,
+        Actors: item.Actors,
+        Poster: item.Poster,
+        Images: item.Images,
+      });
+      initialStatusChanged = false;
+      tableDataRows.innerHTML += `<tr ${
+        !item.Available ? 'class="table-danger"' : ""
+      }>
+      <th scope="row">${item.id}</th>
+      <td>${item.Title}${item.Principal ? `  [⭐️]` : ""}</td>
+      <td>${item.Type === "movie" ? "Pelicula" : "Serie"}</td>
+      <td>${item.Year}</td>
+      <td>${item.Available ? "Si" : "No"}</td>
+      <td><input type="radio" ${item.Principal ? `checked` : " "} 
+      ${!item.Available ? "disabled" : " "}
+      class="fav-movie-radio" name="fav-movie-group" id="radio-${item.id}"></td>
+      <td><button type="button" class="icon-box" data-bs-toggle="modal" data-bs-target="#staticBackdrop"> <img src="../recursos/iconos/edit-alt.svg" alt="edit"></button></td>
+      </tr>`;
     });
-  });
+    let { id: previousFavId } = dbInitialStatus.find((item) => item.fav);
+    dbInfoToChange.push({ previousFavId });
+    checksMovieFav = document.querySelectorAll(".fav-movie-radio");
+    checksMovieFav.forEach((check) => {
+      check.addEventListener("change", (e) =>
+        eventChangeCheck(e, previousFavId)
+      );
+    });
+  } catch (error) {
+    alert(`Ha ocurrido un error: ${e}`);
+  }
 };
 
 let tableDataRows = document.querySelector("#table-data-rows");
@@ -79,10 +95,12 @@ btnSave.addEventListener("click", (e) => {
         "Content-type": "application/json; charset=UTF-8",
       },
     }),
-  ]).then((response) => {
-    if (response.every((res) => res.ok || res.status === 200))
-      window.location.reload();
-  });
+  ])
+    .then((response) => {
+      if (response.every((res) => res.ok || res.status === 200))
+        window.location.reload();
+    })
+    .catch((e) => alert(`Ha ocurrido un error: ${e}`));
 });
 
 window.addEventListener("load", (e) => {
